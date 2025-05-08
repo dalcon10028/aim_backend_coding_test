d298cf1849c86d566fbf28ac81b6b8ef2142f10b
corn <dalcon10280@gmail.com>
2025-05-09 01:33:38 +0900

    refactor: convert all int columns in schema to bigint

--- db/mysql/init/init.sql
+++ db/mysql/init/init.sql
@@ -1,6 +1,6 @@
 create table if not exists users
 (
-    id       int auto_increment primary key,
+    id       bigint auto_increment primary key,
     username varchar(255) not null,
     password varchar(255) not null,
     unique key (username)
@@ -9,9 +9,27 @@ create table if not exists users
 
 create table if not exists login_history
 (
-    id         int auto_increment primary key,
+    id         bigint auto_increment primary key,
     username   varchar(255) not null,
     action     varchar(255) not null comment 'LOGIN, LOGOUT',
+    created_at timestamp default current_timestamp
+) engine = InnoDB
+  default charset = utf8mb4;
+
+create table if not exists account
+(
+    id         bigint auto_increment primary key,
+    user_id    bigint not null,
+    created_at timestamp default current_timestamp
+) engine = InnoDB
+  default charset = utf8mb4;
+
+create table if not exists transaction
+(
+    id         bigint auto_increment primary key,
+    account_id bigint       not null,
+    type       varchar(255) not null comment 'DEPOSIT, WITHDRAW',
+    amount     bigint       not null,
     created_at timestamp default current_timestamp
 ) engine = InnoDB
   default charset = utf8mb4;
